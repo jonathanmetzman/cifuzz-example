229853728c7f2c22fcb5f84a4508b02161401680
Jonathan Metzman <metzman@chromium.org>
2020-04-29 11:42:11 -0700

    fix

--- do_stuff_fuzzer.cpp
+++ do_stuff_fuzzer.cpp
@@ -3,15 +3,14 @@
 #include "my_api.h"
 
 #include <string>
-#include <stdlib>
+#include <stdlib.h>
 
 // Simple fuzz target for DoStuff().
 // See http://libfuzzer.info for details.
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   std::string str(reinterpret_cast<const char *>(data), size);
-  char* x = malloc(1);
+  DoStuff(str);  // Disregard the output.
+  int* x = (int*) malloc(4);
   free(x);
   return x[0];
-  DoStuff(str);  // Disregard the output.
-  return 0;
 }
