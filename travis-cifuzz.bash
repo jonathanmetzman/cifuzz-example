c23ebdf3111e3bbd8c3da6c260fee8fd026f249a
Jonathan Metzman <metzman@chromium.org>
2020-08-06 16:25:10 -0700

    fix

--- travis-cifuzz.bash
+++ travis-cifuzz.bash
@@ -5,8 +5,8 @@ set -x
 git clone https://github.com/google/oss-fuzz.git --depth 1
 docker pull gcr.io/oss-fuzz-base/cifuzz-base:latest
 
-docker build -t build_fuzzers oss-fuzz/infra/cifuzz/actions/build_fuzzers
-docker build -t run_fuzzers oss-fuzz/infra/cifuzz/actions/run_fuzzers
+docker build -t gcr.io/oss-fuzz-base/build_fuzzers oss-fuzz/infra/cifuzz/actions/build_fuzzers
+docker build -t gcr.io/oss-fuzz-base/run_fuzzers oss-fuzz/infra/cifuzz/actions/run_fuzzers
 
 # export GITHUB_REPOSITORY=$REPO_NAME
 export GITHUB_REPOSITORY=$REPO_NAME
@@ -21,6 +21,6 @@ export WORKDIR='/tmp/cifuzz'
 mkdir $WORKDIR
 export GITHUB_WORKSPACE=$WORKDIR
 
-docker run --name build_fuzzers --rm -ti -e GITHUB_WORKSPACE -e GITHUB_REPOSITORY -e GITHUB_EVENT_NAME -e DRY_RUN -e CI -e SANITIZER -e GITHUB_SHA -v /var/run/docker.sock:/var/run/docker.sock -v $WORKDIR:$WORKDIR build_fuzzers
+docker run --name build_fuzzers --rm -ti -e GITHUB_WORKSPACE -e GITHUB_REPOSITORY -e GITHUB_EVENT_NAME -e DRY_RUN -e CI -e SANITIZER -e GITHUB_SHA -v /var/run/docker.sock:/var/run/docker.sock -v $WORKDIR:$WORKDIR gcr.io/oss-fuzz-base/build_fuzzers
 
-docker run --name run_fuzzers --rm -ti -e GITHUB_WORKSPACE -e GITHUB_REPOSITORY -e GITHUB_EVENT_NAME -e DRY_RUN -e CI -e SANITIZER -e GITHUB_SHA -v /var/run/docker.sock:/var/run/docker.sock -v $WORKDIR:$WORKDIR run_fuzzers
+docker run --name run_fuzzers --rm -ti -e GITHUB_WORKSPACE -e GITHUB_REPOSITORY -e GITHUB_EVENT_NAME -e DRY_RUN -e CI -e SANITIZER -e GITHUB_SHA -v /var/run/docker.sock:/var/run/docker.sock -v $WORKDIR:$WORKDIR gcr.io/oss-fuzz-base/run_fuzzers
