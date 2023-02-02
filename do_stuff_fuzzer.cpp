f9e8045e82647332f64e4f22e92222da7fb9ece1
Jonathan Metzman <metzman@chromium.org>
2023-02-02 11:55:03 -0500

    fix

--- do_stuff_fuzzer.cpp
+++ do_stuff_fuzzer.cpp
@@ -8,7 +8,11 @@
 // Simple fuzz target for DoStuff().
 // See http://libfuzzer.info for details.
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+  if (size < 4) {
+    return 0;
+  }
   std::string str(reinterpret_cast<const char *>(data), size);
   DoStuff(str);  // Disregard the output.
   vuln(data[0]);
+  return 0;
 }
